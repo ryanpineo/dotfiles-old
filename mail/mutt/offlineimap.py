ae667aec822323e4ea4b3ba900c3309cc6222da1
Ryan Pineo <ryanpineo@gmail.com>
2014-12-08 23:45:05 -0500

    use "head" in email password getter

--- mail/mutt/offlineimap.py
+++ mail/mutt/offlineimap.py
@@ -5,14 +5,14 @@ import sys
 
 
 def get_email_password(name):
-    name_path_map = {'gmail': 'email/offlineimap-ryanpineo-gmail.com',
+    name_path_map = {'gmail': 'email/ryanpineo@gmail.com',
                      'tsl': 'tsl/email',
                      'school': 'sheridan/sso'}
 
-    command = 'pass {}'.format(name_path_map[name])
+    command = 'pass {} | head -1'.format(name_path_map[name])
     output = subprocess.check_output(command, shell=True,
                                      stderr=subprocess.STDOUT)
-    return output.split('\n')[0].strip().decode('ascii')
+    return output.strip().decode('ascii')
 
 if __name__ == '__main__':
     if len(sys.argv) == 2:
